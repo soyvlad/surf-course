f7d4d9ec9a126477c605f6ade55843b949eda3cb
soyvlad <soyvaldis@gmail.com>
2024-03-21 17:37:33 +0300

    Добавил именованные параметры

--- task-7/goods_filter/lib/main.dart
+++ task-7/goods_filter/lib/main.dart
@@ -1,4 +1,4 @@
-// определение класса Article (товар) с полями id, type, name, price, quantity
+// Определение класса Article (товар) с полями id, type, name, price, quantity.
 class Article {
   final int id;
   final String type;
@@ -6,82 +6,110 @@ class Article {
   final double price;
   final int quantity;
 
-// конструктор класса Article, который принимает значения id, type, name, price, quantity и инициализирует соответствующие поля
-  Article(this.id, this.type, this.name, this.price, this.quantity);
+// Конструктор класса Article, который принимает значения id, type, name, price, quantity и инициализирует соответствующие поля.
+  Article({
+    required this.id,
+    required this.type,
+    required this.name,
+    required this.price,
+    required this.quantity,
+  });
+
+  @override
+  String toString() => '$id\t $type\t $name\t $price рублей\t $quantity шт.';
 }
 
-// определение абстрактного класса Filter
+// Определение абстрактного класса Filter.
 abstract class Filter {
-// объявление абстрактного метода apply, который принимает объект класса Article и возвращает булево значение
+// Объявление абстрактного метода apply, который принимает объект класса Article и возвращает булево значение.
   bool apply(Article article);
 }
 
-// определение класса TypeFilter, который реализует интерфейс Filter
+// Определение класса TypeFilter, который реализует интерфейс Filter.
 class TypeFilter implements Filter {
   final String type;
-// конструктор класса TypeFilter, принимающий значение type и инициализирующий соответствующее поле
+// Конструктор класса TypeFilter, принимающий значение type и инициализирующий соответствующее поле.
   TypeFilter(this.type);
-// переопределение метода apply из интерфейса Filter в классе TypeFilter, который проверяет соответствие типа товара
+// Переопределение метода apply из интерфейса Filter в классе TypeFilter, который проверяет соответствие типа товара.
   @override
   bool apply(Article article) {
     return article.type == type;
   }
 }
 
-// определение класса PriceFilter, который реализует интерфейс Filter
+// Определение класса PriceFilter, который реализует интерфейс Filter.
 class PriceFilter implements Filter {
   final double maxPrice;
-// конструктор класса PriceFilter, принимающий значение maxPrice и инициализирующий соответствующее поле
+// Конструктор класса PriceFilter, принимающий значение maxPrice и инициализирующий соответствующее поле.
   PriceFilter(this.maxPrice);
 
-// переопределение метода apply из интерфейса Filter в классе PriceFilter, который проверяет цену товара
+// Переопределение метода apply из интерфейса Filter в классе PriceFilter, который проверяет цену товара.
   @override
   bool apply(Article article) {
     return article.price <= maxPrice;
   }
 }
 
-// определение класса QuantityFilter, который реализует интерфейс Filter
+// Определение класса QuantityFilter, который реализует интерфейс Filter.
 class QuantityFilter implements Filter {
   final int maxQuantity;
-// конструктор класса QuantityFilter, принимающий значение maxQuantity и инициализирующий соответствующее поле
+// Конструктор класса QuantityFilter, принимающий значение maxQuantity и инициализирующий соответствующее поле.
   QuantityFilter(this.maxQuantity);
-// переопределение метода apply из интерфейса Filter в классе QuantityFilter, который проверяет количество товара
+// Переопределение метода apply из интерфейса Filter в классе QuantityFilter, который проверяет количество товара.
   @override
   bool apply(Article article) {
     return article.quantity >= maxQuantity;
   }
 }
 
-// определение функции applyFilter, которая принимает список продуктов и фильтр, применяет фильтр к каждому товару и выводит результаты
+// Определение функции applyFilter, которая принимает список продуктов и фильтр, применяет фильтр к каждому товару и выводит результаты.
 void applyFilter(List<Article> articles, Filter filter) {
-  print('Id\tType\tName\tPrice\tQuantity');
-
-  for (var article in articles) {
+  final filteredArticles = articles.where(filter.apply).toList();
+  print('Id:\t Категория:\t Наименование:\t Цена:\t Количество:');
+  for (var article in filteredArticles) {
     if (filter.apply(article)) {
-      print(
-          '${article.id}\t ${article.type}\t ${article.name}\t ${article.price} рублей\t  ${article.quantity} шт.');
+      print(article.toString());
     }
   }
 }
 
-// определение функции main, в которой создается список продуктов, применяются различные фильтры к этому списку и результаты выводятся на экран
+// Определение функции main, в которой создается список продуктов, применяются различные фильтры к этому списку и результаты выводятся на экран.
 void main() {
-  List<Article> articleList = [
-    Article(1, 'хлеб', 'Бородинский', 500, 5),
-    Article(2, 'хлеб', 'Белый', 200, 15),
-    Article(3, 'молоко', 'Полосатый кот', 50, 53),
-    Article(4, 'молоко', 'Коровка', 50, 53),
-    Article(5, 'вода', 'Апельсин', 25, 100),
-    Article(6, 'вода', 'Бородинский', 500, 5)
-  ];
+  final articles = '''
+    1,хлеб,Бородинский,500,5
+    2,хлеб,Белый,200,15
+    3,молоко,Полосатый кот,50,53
+    4,молоко,Коровка,50,53
+    5,вода,Апельсин,25,100
+    6,вода,Бородинский,500,5
+  ''';
+  final articleList = parseArticles(articles);
 
-  print('Фильтрация по категории: хлеб');
+  print('\n Фильтрация по категории "хлеб": \n');
   applyFilter(articleList, TypeFilter('хлеб'));
 
-  print('Фильтрация по цене не дороже 300 рублей');
+  print('\n Фильтрация по цене не дороже 300 рублей: \n');
   applyFilter(articleList, PriceFilter(300));
 
-  print('Фильтрация по количеству не меньше 10 шт.');
+  print('\n Фильтрация по количеству не меньше 10 шт.: \n');
   applyFilter(articleList, QuantityFilter(10));
+}
+
+List<Article> parseArticles(String articlesString) {
+  final List<Article> result = [];
+  final lines = articlesString.trim().split('\n');
+  for (var line in lines) {
+    final parts = line.trim().split(',');
+    if (parts.length == 5) {
+      final id = int.parse(parts[0]);
+      final type = parts[1];
+      final name = parts[2];
+      final price = double.parse(parts[3]);
+      final quantity = int.parse(parts[4]);
+      final article = Article(
+          id: id, type: type, name: name, price: price, quantity: quantity);
+      result.add(article);
+    }
+  }
+  return result;
 }
